0d8dc3c071bb64b62cda03a48cfc8a9c18b88fc4
Che-Lun Juang <clj327@nyu.edu>
2018-09-28 13:20:55 -0400

    Use chart.js to draw basic chart

--- src/components/result/summary.jsx
+++ src/components/result/summary.jsx
@@ -7,21 +7,8 @@ import _ from "lodash";
 import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import {getSummary} from "../../actions";
-import * as d3 from "d3";
-import CHART_STYLE from "!!raw-loader!sass-loader!../../styles/chart_style.scss";
-
-function formatValues(d) {
-  let vals = _.values(d);
-  let [init, ...rest] = vals;
-  let cumVals = _.reduce(rest, (o, v) => [...o, o[o.length - 1] + v], [init]);
-
-  return _.zip(_.keys(d), vals, cumVals);
-}
-
-const sumVals = _.unary(_.flow(
-  _.values,
-  _.sum
-));
+import Chart from "chart.js";
+import palette from "google-palette";
 
 function mapStateToProps({requestId, summary}) {
   return {
@@ -34,7 +21,7 @@ class SummaryBody extends React.Component {
   constructor(props) {
     super(props);
 
-    this.svg = React.createRef();
+    this.chartCtx = React.createRef();
 
     this.state = {
       height: 0,
@@ -42,51 +29,84 @@ class SummaryBody extends React.Component {
     };
 
     this.setHeight = _.debounce(this.setHeight.bind(this));
-
-    this.margin = {
-      top: 20,
-      right: 30,
-      bottom: 30,
-      left: 40
-    };
   }
 
   componentDidMount() {
     this.setHeight();
     window.addEventListener('resize', this.setHeight);
 
-    let {top, bottom, left} = this.margin;
-    let {height} = this.state;
-    height -= top + bottom;
+    this.props.getSummary(this.props.requestId);
 
-    this.chart = d3.select(this.svg.current)
-      .attr("height", height + top + bottom)
-      .append("g")
-      .attr("transform", `translate(${left},${top})`);
+    this.chart = new Chart(this.chartCtx.current.getContext("2d"), {
+      type: 'bar',
+      options: {
+        title: {
+          display: true,
+          text: 'Summary'
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: true
+        },
+        maintainAspectRatio: false,
+        scales: {
+          xAxes: [{
+            stacked: true,
+            ticks: {
+              callback: function (value) {
+                // needs improvement
+                return value.slice(0, 9)
+              }
+            }
+          }],
+          yAxes: [{
+            stacked: true
+          }]
+        }
+      }
+    });
 
-    this.props.getSummary(this.props.requestId)
-      .then(() => {
-        this.draw();
-      });
+    this.updateChart();
   }
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps) {
     if (this.props.summary !== prevProps.summary) {
-      // screw exit, we'll do the exit ourselves
-      this.chart.selectAll("g").remove();
-    }
-
-    if (!_.isEmpty(this.props.summary) && (this.state.height !== prevState.height || this.state.width !== prevState.width)) {
-      this.draw();
+      this.updateChart();
     }
   }
 
   componentWillUnmount() {
     window.removeEventListener('resize', this.setHeight);
+    this.chart.destroy();
+  }
+
+  updateChart() {
+    let {summary: {chart}} = this.props;
+
+    let chartVals = _(chart).values();
+    let chartKeys = chartVals.map(_.keys).flatten().sortBy().uniq();
+
+    this.chart.data = {
+      labels: _.keys(chart),
+      datasets: chartKeys.zipWith(
+        chartKeys.map((key) => chartVals.map(key).value()).value(),
+        palette('tol', chartKeys.size()),
+        (key, val, color) => {
+          return {
+            label: key,
+            backgroundColor: '#' + color,
+            stack: 'a',
+            data: val
+          };
+        }
+      ).value()
+    };
+
+    this.chart.update()
   }
 
   setHeight() {
-    let {top, width} = this.svg.current.getBoundingClientRect();
+    let {top, width} = this.chartCtx.current.getBoundingClientRect();
 
     this.setState({
       height: Math.max(document.documentElement.clientHeight, window.innerHeight || 0) - top,
@@ -94,82 +114,14 @@ class SummaryBody extends React.Component {
     });
   }
 
-  draw() {
-    let {top, right, bottom, left} = this.margin;
-    let {height, width} = this.state;
-    // let width = 960 - left - right;
-
-    width -= left + right;
-    height -= top + bottom;
-
-    d3.select(this.svg.current)
-      .attr("height", height + top + bottom);
-
-    let {summary} = this.props;
-
-    let keys = _.keys(summary.chart);
-    let values = _.values(summary.chart);
-
-    let x = d3.scaleBand().padding(0.5).domain(keys).range([0, width]);
-    let y = d3.scaleLinear().domain([0, _(values).map(sumVals).max()]).range([height, 0]);
-
-    let bar = this.chart.selectAll(".bar-group")
-      .data(values);
-
-    let barEnter = bar.enter()
-      .append("g")
-      .attr("class", "bar-group")
-      .merge(bar)
-      .attr("transform", (d, i) => `translate(${x(keys[i])}, 0)`);
-
-    let innerKeys = _(values).map(_.keys).flatten().uniq().sortBy().value();
-
-    let c = d3.scaleOrdinal(d3.schemeCategory10).domain(innerKeys);
-
-    let subBars = barEnter.selectAll("rect")
-      .data(formatValues);
-
-    let subBarEnter = subBars
-      .enter()
-      .append("rect")
-      .attr("fill", (d) => c(d[0]))
-      .attr("class", "bar");
-
-    subBarEnter.merge(subBars)
-      .attr("y", function (d) {
-        return y(d[2]);
-      })
-      .attr("height", function (d) {
-        return height - y(d[1]);
-      })
-      .attr("width", x.bandwidth());
-
-    let xAxis = d3.axisBottom(x);
-
-    let ax = this.chart.select("g.x");
-    ax = ax.size() ? ax : this.chart.append("g");
-
-    ax
-      .attr("class", "x axis")
-      .attr("transform", `translate(0,${height})`)
-      .call(xAxis);
-
-    console.log(ax);
-  }
-
   render() {
-    // let {height} = this.state;
-    let {summary} = this.props;
+    let {height} = this.state;
 
     return <div>
       <div>Riveting content.</div>
-      <svg className="w-100 chart" ref={this.svg}
-           xmlns="http://www.w3.org/2000/svg"
-           xmlnsXlink="http://www.w3.org/1999/xlink">
-        <style>
-          {CHART_STYLE}
-        </style>
-      </svg>
+      <div className="w-100" style={{height}}>
+        <canvas ref={this.chartCtx}/>
+      </div>
     </div>;
   }
 }
