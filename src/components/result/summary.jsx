abf595a2159d65c2d7ee6675a13529f0bd7a3b4c
Che-Lun Juang <clj327@nyu.edu>
2018-10-02 10:59:02 -0400

    Summary chart
    Proper formatting and labeling of chart

--- src/components/result/summary.jsx
+++ src/components/result/summary.jsx
@@ -37,6 +37,7 @@ class SummaryBody extends React.Component {
 
     this.props.getSummary(this.props.requestId);
 
+    // hack-tastic way of displaying a grouped bar chart to our specification
     this.chart = new Chart(this.chartCtx.current.getContext("2d"), {
       type: 'bar',
       options: {
@@ -45,8 +46,40 @@ class SummaryBody extends React.Component {
           text: 'Summary'
         },
         tooltips: {
-          mode: 'index',
-          intersect: true
+          mode: 'x',
+          position: 'nearest',
+          intersect: true,
+          filter: function (tooltipItem) {
+            return tooltipItem.yLabel > 0;
+          },
+          callbacks: {
+            title: function (tooltipItems, data) {
+              return _.get(data.datasets, [_.get(tooltipItems, '0.datasetIndex'), 'tooltip']);
+            }
+          }
+        },
+        legend: {
+          labels: {
+            filter: function (legendItem, data) {
+              // only show first of same label
+              return _.findIndex(data.datasets, ["label", legendItem.text]) === legendItem.datasetIndex;
+            }
+          },
+          onClick: function (e, legendItem) {
+            let label = legendItem.text;
+            let index = legendItem.datasetIndex;
+            let ci = this.chart;
+
+            // hide all with same label at once
+            _(_.range(ci.data.datasets.length))
+              .filter((i) => ci.data.datasets[i].label === label)
+              .map(ci.getDatasetMeta.bind(ci))
+              .forEach(function (meta) {
+                meta.hidden = meta.hidden === null ? !ci.data.datasets[index].hidden : null;
+              });
+
+            ci.update();
+          }
         },
         maintainAspectRatio: false,
         scales: {
@@ -55,7 +88,7 @@ class SummaryBody extends React.Component {
             ticks: {
               callback: function (value) {
                 // needs improvement
-                return value.slice(0, 9)
+                return /^[^\s,]+/.exec(value)[0];
               }
             }
           }],
@@ -83,26 +116,44 @@ class SummaryBody extends React.Component {
   updateChart() {
     let {summary: {chart}} = this.props;
 
-    let chartVals = _(chart).values();
-    let chartKeys = chartVals.map(_.keys).flatten().sortBy().uniq();
+    let _chart = _(chart);
+    let chartVals = _chart.values();
+    let chartEdges = chartVals.map(_.keys).flatten().sortBy().uniq();
+    let chartKeys = _chart.keys()
+      .map((k) => {
+        let i = k.indexOf(',');
+        return [k.substr(0, i), k.substr(i + 1)];
+      });
+
+    let chartObj = _(chartKeys.zip(chartVals.value()).reduce((c, o) => _.set(c, o[0], o[1]), {}));
+
+    let chartSize = chartObj.size();
 
     this.chart.data = {
-      labels: _.keys(chart),
-      datasets: chartKeys.zipWith(
-        chartKeys.map((key) => chartVals.map(key).value()).value(),
-        palette('tol', chartKeys.size()),
-        (key, val, color) => {
-          return {
-            label: key,
-            backgroundColor: '#' + color,
-            stack: 'a',
-            data: val
-          };
+      labels: chartObj.keys().value(),
+      datasets: chartEdges.zipWith(
+        palette('tol', chartEdges.size()),
+        (edge, color) => {
+          return chartObj.values().map((val, i) => {
+            return _(val).toPairs().map(([k, v], j) => {
+              let arr = Array(chartSize);
+              _.fill(arr, 0);
+              arr[i] = v[edge];
+
+              return {
+                label: edge,
+                backgroundColor: '#' + color,
+                stack: j,
+                data: arr,
+                tooltip: k
+              };
+            }).filter((o) => o.data[i]).value();
+          }).value();
         }
-      ).value()
+      ).flattenDeep().value()
     };
 
-    this.chart.update()
+    this.chart.update();
   }
 
   setHeight() {
@@ -118,7 +169,6 @@ class SummaryBody extends React.Component {
     let {height} = this.state;
 
     return <div>
-      <div>Riveting content.</div>
       <div className="w-100" style={{height}}>
         <canvas ref={this.chartCtx}/>
       </div>
